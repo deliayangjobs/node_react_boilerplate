7d4bbd86d37578803fabf46c10fa079a6d960481
Delia Yang <deliay@surveymonkey.com>
2018-05-22 15:27:45 -0700

    server code refactor

---
+++ routes/authRoutes.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+module.exports = (app) => {
+
+    // when user visit /auth/google, we handover user to passport
+    // to authenticate user, use strategy 'google', GoogleStrategy has
+    // an internal identifier as 'google', thats why can use here
+    // options.scope ask google get user's profile and email info
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    // when user get direct back to callback from signin with google
+    // here has code info available, passport will exchange the code
+    // for user profile
+    app.get('/auth/auth/google/callback', passport.authenticate('google'));
+};
