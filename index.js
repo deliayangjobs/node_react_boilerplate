f1e3e173bb46b409a69f4e682553fac19a0bca37
Delia Yang <deliay@surveymonkey.com>
2018-06-06 15:41:42 -0700

    create stripe charge and add credit to user model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport'); // just to execute passport.js code
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 // enable cookies in express
 app.use(
     cookieSession({
@@ -18,9 +20,10 @@ app.use(
     })
 );
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
