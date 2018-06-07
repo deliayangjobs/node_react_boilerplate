f80f9a666bf2b54cea0611100970d97a75c2b8c9
Delia Yang <deliay@surveymonkey.com>
2018-06-07 11:38:31 -0700

    config heroku nodejs run npm run build for client

--- index.js
+++ index.js
@@ -25,5 +25,20 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // routes other than authRoutes and billingRoutes above
+    // like /client/build/static/js/main.js
+    app.use(express.static('client/build'));
+
+    // routes other than find specific file under client/build
+    // like /surveys
+    const path = require('path');
+    app.get('*', (req, res) => {
+        // __dirname gives you the path of the currently running file
+        // path.resolve return an absolute path
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
